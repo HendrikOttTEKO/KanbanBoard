4d71849c00421ce11acc2ab4c407ebf38b512691
Yaser Daqiq <yasir.dag@live.de>
2026-07-15 20:40:25 +0200

    GET und POST API für Tasks erstellt

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,42 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+const tasks = [
+  {
+    id: 1,
+    title: "Backend einrichten",
+    status: "To Do",
+    priority: "Hoch",
+  },
+  {
+    id: 2,
+    title: "Kanban Board gestalten",
+    status: "In Progress",
+    priority: "Mittel",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Kanban Board Backend läuft im Entwicklingsmodus!");
+});
+
+app.get("/api/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+app.post("/api/tasks", (req, res) => {
+  const newTask = {
+    id: tasks.length + 1,
+    title: req.body.title,
+    status: req.body.status,
+    priority: req.body.priority,
+  };
+
+  tasks.push(newTask);
+
+  res.status(201).json(newTask);
 });
 
 app.listen(PORT, () => {
